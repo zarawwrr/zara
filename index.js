6d23ca86bb5db0861812774f8c9b71fafd63eb2c
zarawwrr <164300456+zarawwrr@users.noreply.github.com>
2024-09-10 10:45:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+const myExam = {
+    subject: "ITE314",
+    exam: "P2",
+    node: "NODEJS",
+};
+
+app.get('/', (req, res) =>{
+    res.send("Welcome to my server! To learn more, try putting any of the following to the URL: ('/subject', '/exam', '/node')")
+})
+app.get('/subject', (req, res) => {
+    res.send(myExam.subject);
+});
+
+
+app.get('/exam', (req, res) => {
+    res.send(myExam.exam);
+});
+
+
+app.get('/node', (req, res) => {
+    res.send(myExam);
+});
+
+
+app.get('/mydata', (req, res) => {
+    res.send(myExam)
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
